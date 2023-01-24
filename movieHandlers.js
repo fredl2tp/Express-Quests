44ef84f6c28dc2da4640b5b743a4db967eb1d5ae
fredl2tp <fleschira@gmail.com>
2023-01-24 11:40:13 +0100

    Quest Express 03

--- movieHandlers.js
+++ movieHandlers.js
@@ -73,8 +73,27 @@ const getMovieById = (req, res) => {
     });
 };
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      // wait for it
+      //en-tête Location pointant vers la nouvelle ressource et statut HTTP "Created"
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
 
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
 };
